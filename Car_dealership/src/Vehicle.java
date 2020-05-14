e6c191486aceed0b4c3888bdf71e157d5b26e1d9
Mikiyas-ahmed <mikiyasahmed7@gmail.com>
2020-05-14 19:14:07 +0200

    creating vehicle information and encapsulation

--- Car_dealership/src/Vehicle.java
+++ Car_dealership/src/Vehicle.java
@@ -1,4 +1,33 @@
 
 public class Vehicle {
+		
+	private int price;
+	private String carColoure;
+	private String corModel;
+		
+		public String getCarColoure() {
+		return carColoure;
+	}
 
+	public void setCarColoure(String carColoure) {
+		this.carColoure = carColoure;
+	}
+
+	public String getCorModel() {
+		return corModel;
+	}
+
+	public void setCorModel(String corModel) {
+		this.corModel = corModel;
+	}
+
+		public int getPrice() {
+			return price;
+		}
+		
+		public void setPrice(int price) {
+			this.price = price;
+		}
+		
+		
 }
